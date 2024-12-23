5a04658aebec367573bb183e65ecb189fab34965
ruhul-7451 <rokeyayasminmim17official@gmail.com>
2024-12-23 23:00:17 +0600

    appbar theme has been added

---
+++ lib/utils/theme/customs_theme/appbar_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppbarTheme {
+  AppbarTheme._();
+  static const lightAppbarTheme = AppBarTheme(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      scrolledUnderElevation: 0,
+      centerTitle: false,
+      foregroundColor: Colors.transparent,
+      iconTheme: IconThemeData(color: Colors.black, size: 24),
+      actionsIconTheme: IconThemeData(color: Colors.black, size: 24),
+      titleTextStyle: TextStyle(
+          fontSize: 18, fontWeight: FontWeight.w600, color: Colors.black));
+  static const darkAppbarTheme = AppBarTheme(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      scrolledUnderElevation: 0,
+      centerTitle: false,
+      foregroundColor: Colors.transparent,
+      iconTheme: IconThemeData(color: Colors.black, size: 24),
+      actionsIconTheme: IconThemeData(color: Colors.black, size: 24),
+      titleTextStyle: TextStyle(
+          fontSize: 18, fontWeight: FontWeight.w600, color: Colors.black));
+}
