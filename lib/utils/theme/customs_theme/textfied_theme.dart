9e15de8d18bcb94093dcd09c8ddbabe7118a43e8
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2025-01-03 21:02:01 +0600

    textfieldtheme added.

---
+++ lib/utils/theme/customs_theme/textfied_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class TextfromfieldTheme {
+  TextfromfieldTheme._();
+
+  static InputDecorationTheme lightInputdecorationtheme = InputDecorationTheme(errorMaxLines: 3,prefixIconColor: Colors.grey,suffixIconColor: Colors.grey,enabledBorder: const OutlineInputBorder().copyWith(borderRadius: BorderRadius.circular(14),borderSide:const BorderSide(color: Colors.grey,width: 1)),border: const OutlineInputBorder().copyWith(borderRadius: BorderRadius.circular(10),borderSide: const BorderSide(width: 1,color: Colors.grey)),errorBorder: 
+  const OutlineInputBorder().copyWith(borderRadius: BorderRadius.circular(10),borderSide: const BorderSide(color: Colors.red,width: 1)),focusedBorder: const OutlineInputBorder().copyWith(borderRadius: BorderRadius.circular(10),borderSide: const BorderSide(color: Colors.black)),hintStyle:const TextStyle().copyWith(color: Colors.black,fontSize: 14,),errorStyle:const TextStyle().copyWith(fontStyle: FontStyle.normal,) );
+  
+}                                                                                                                                                                                                                                                                       
