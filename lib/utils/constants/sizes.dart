189ad5ff5480477db5ba8a9f60ecc04d2e7dec7d
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2025-01-05 23:37:06 +0600

    sizes dart file added

---
+++ lib/utils/constants/sizes.dart
@@ -0,0 +1,24 @@
+class Sizes {
+  //padding and margin          
+  static  const double paddingXs =4.0;
+  static  const double paddingSm =8.0;
+  static  const double paddingMd =16.0;
+  static  const double paddingLg =24.0;
+  static  const double paddingXl =32.0;
+
+  //icon
+   static  const double IconXs =4.0;
+  static  const double IconSm =8.0;
+  static  const double IconMd =16.0;
+  static  const double IconLg =24.0;
+  static  const double IconXl =32.0;
+
+
+//font 
+   static  const double fontSizeXs =4.0;
+  static  const double fontSizeSm =8.0;
+  static  const double fontSizeMd =16.0;
+  static  const double fontSizeLg =24.0;
+  static  const double fontSizeXl =32.0;
+
+}
